1ba58407cf708a4c2782423f827f619be1fe62b6
IrishLaluz <ishielaluz@gmail.com>
2025-11-29 12:51:57 +0000

    Add package.jason

--- server.js
+++ server.js
@@ -1,12 +1,43 @@
 const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
 const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-  res.write("Hello Cloud!!!");
-  res.write("Welcome to my page.");
-  res.end();
+    
+    // Serve index.html
+    if (req.url === "/" || req.url === "/index.html") {
+        fs.readFile("./index.html", (err, data) => {
+            if (err) {
+                res.writeHead(500, { "Content-Type": "text/plain" });
+                return res.end("Error loading page");
+            }
+            res.writeHead(200, { "Content-Type": "text/html" });
+            res.end(data);
+        });
+    }
+
+    // Serve styles.css  <-- updated filename
+    else if (req.url === "/styles.css") {
+        fs.readFile("./styles.css", (err, data) => {
+            if (err) {
+                res.writeHead(404, { "Content-Type": "text/plain" });
+                return res.end("CSS file not found");
+            }
+            res.writeHead(200, { "Content-Type": "text/css" });
+            res.end(data);
+        });
+    }
+
+    // All other routes
+    else {
+        res.writeHead(404, { "Content-Type": "text/plain" });
+        res.end("404 Not Found");
+    }
+
 });
 
 server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
